47abcc9aaad87c12bb027c1b61e9002c415db6b4
Yuqimak <79020393+Yuqimak@users.noreply.github.com>
2026-04-28 23:31:40 +0700

    Add basic functionality for lamp control

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.getElementById("status").innerText = "Online";
+document.getElementById("temp").innerText = "28 °C";
+
+function turnOn() {
+  document.getElementById("lamp").innerText = "ON";
+}
+
+function turnOff() {
+  document.getElementById("lamp").innerText = "OFF";
+}
