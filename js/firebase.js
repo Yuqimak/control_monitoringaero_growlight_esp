54e1b1fa9f2c7b25a4c3a0d752ddc3a690d0668b
Yuqimak <79020393+Yuqimak@users.noreply.github.com>
2026-07-09 15:09:25 +0700

    Refactor Firebase import and export statements

--- js/firebase.js
+++ js/firebase.js
@@ -1,31 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-
-import {
-  getDatabase
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSy...", // Ganti dengan API Key asli
   authDomain: "growlightta.firebaseapp.com",
-
-  databaseURL:
-    "https://growlightta-default-rtdb.asia-southeast1.firebasedatabase.app",
-
+  databaseURL: "https://growlightta-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "growlightta",
-
-  storageBucket:
-    "growlightta.firebasestorage.app",
-
+  storageBucket: "growlightta.firebasestorage.app",
   messagingSenderId: "982821946750",
-
-  appId:
-    "1:982821946750:web:98fc04e2b573e9dd955f2f"
-
+  appId: "1:982821946750:web:98fc04e2b573e9dd955f2f"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getDatabase(app);
 
 export { db };
